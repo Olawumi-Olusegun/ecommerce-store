e511e868620e58dae0a71b89d6f5ddbdc7cd84b4
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-09-08 11:12:58 +0100

    Fixed: refresh route url

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -4,20 +4,20 @@ const baseURL = import.meta.mode === "development" ? "http://localhost:5000/api"
 
     const axiosInstance = axios.create({ baseURL, withCredentials: true });
 
-    const callRefreshTokenApi = async () => await axios.get(`${baseURL}/v1/auth/refresh-token`, { withCredentials: true });
-
+    const callRefreshTokenApi = async () => await axios.post(`${baseURL}/v1/auth/refresh-token`, { withCredentials: true });
 
     axiosInstance.interceptors.response.use(async(response) => {
 
         return response;
-      
+
       }, async (error) => {
       
           if(error?.response?.status === 401) {
-      
+
             try {
       
               const responseResult = await callRefreshTokenApi();
+
               localStorage.setItem('auth-user', JSON.stringify(responseResult?.data?.user));
 
             } catch (refreshTokenError) {
@@ -28,7 +28,7 @@ const baseURL = import.meta.mode === "development" ? "http://localhost:5000/api"
             return axios(error?.config);
       
           }
-      
+
           return Promise.reject(error?.message);
       
       });
